def7c3bfb8f0ab8fc8bc11d9dd1f076baa079437
yostane <yostane@yahoo.fr>
2018-04-22 20:46:40 +0200

    Added reactive forms project

--- angular-forms/src/app/hero-form/hero-form.component.ts
+++ angular-forms/src/app/hero-form/hero-form.component.ts
@@ -7,17 +7,22 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent implements OnInit {
-
   powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
   submitted = false;
-  onSubmit() { this.submitted = true; }
+  onSubmit() {
+    this.submitted = true;
+  }
   // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
-
-  constructor() { }
-
-  ngOnInit() {
+  get diagnostic() {
+    return JSON.stringify(this.model);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
 }
